472107dd100adc178504c060d3dcf949e39653a0
rocky <rrrrrrrrrrrocky@gmail.com>
2024-05-10 17:06:45 +0900

    fix: react version 수정

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -14,7 +14,7 @@ module.exports = {
           // 명시하지 않을 경우(기본값 'detect') React 라이브러리 전체를 불러오므로
           // 린트 과정에서 속도가 느려질 수 있습니다.
           // 예: '16.9', '17.0', '18.0' 등
-          version: 'latest',
+          version: 'detect',
         },
       },
       parserOptions: {
