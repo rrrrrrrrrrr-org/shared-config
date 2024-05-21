1976c88062cae1a6736e057ec5d1e001d1f0a7e1
rocky <rrrrrrrrrrrocky@gmail.com>
2024-05-21 12:27:36 +0900

    feat: npm publish workflow 추가 - 1

---
+++ scripts/start-release.js
@@ -0,0 +1,85 @@
+const path = require("path");
+const execa = require("execa");
+const resolveFrom = require("resolve-from");
+
+const SEMVER_TYPES = ["patch", "minor", "major"];
+
+async function main() {
+  const args = process.argv;
+  const releaseType = args[args.indexOf("--release-type") + 1];
+  const semverType = args[args.indexOf("--semver-type") + 1];
+  const isCanary = releaseType !== "stable";
+
+  if (releaseType !== "stable" && releaseType !== "canary") {
+    console.log(
+      `Invalid release type ${releaseType}, must be stable or canary`
+    );
+    return;
+  }
+  if (!isCanary && !SEMVER_TYPES.includes(semverType)) {
+    console.log(
+      `Invalid semver type ${semverType}, must be one of ${SEMVER_TYPES.join(
+        ", "
+      )}`
+    );
+    return;
+  }
+
+  const githubToken = process.env.GITHUB_TOKEN;
+  const npmToken = process.env.NPM_TOKEN;
+
+  if (!githubToken) {
+    console.log(`Missing GITHUB_TOKEN`);
+    return;
+  }
+
+  if (!npmToken) {
+    console.log(`Missing NPM_TOKEN`);
+    return;
+  }
+
+  const configStorePath = resolveFrom(
+    path.join(process.cwd(), "node_modules/release"),
+    "configstore"
+  );
+  const ConfigStore = require(configStorePath);
+
+  const config = new ConfigStore("release");
+  config.set("token", githubToken);
+
+  await execa(
+    `git remote set-url origin https://x-access-token:${githubToken}@github.com/rrrrrrrrrrr-org/shared-config.git`,
+    { stdio: "inherit", shell: true }
+  );
+  await execa(`git config user.name "github-actions[bot]"`, {
+    stdio: "inherit",
+    shell: true,
+  });
+  await execa(
+    `git config user.email "41898282+github-actions[bot]@users.noreply.github.com"`,
+    {
+      stdio: "inherit",
+      shell: true,
+    }
+  );
+
+  console.log(`Running pnpm release-${isCanary ? "canary" : "stable"}...`);
+  const child = execa(
+    isCanary
+      ? `pnpm lerna version ${
+          semverType === "minor" ? "preminor" : "prerelease"
+        } --preid canary --force-publish -y && pnpm release --pre --skip-questions --show-url`
+      : `pnpm lerna version ${semverType} --force-publish -y`,
+    {
+      stdio: "pipe",
+      shell: true,
+    }
+  );
+
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+  await child;
+  console.log("Release process is finished");
+}
+
+main();
